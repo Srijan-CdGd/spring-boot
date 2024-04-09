5d2f1109f6310adadbf8bd1015a35e2e00cb8c77
Srijan Airon <61419096+Srijan-CdGd@users.noreply.github.com>
2024-04-09 12:58:16 +0530

    logout added

--- spring_security_jwt/src/main/java/com/spring/security/jwt/learn/spring_security_jwt/config/SecurityConfig.java
+++ spring_security_jwt/src/main/java/com/spring/security/jwt/learn/spring_security_jwt/config/SecurityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,6 +28,7 @@ public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final LogoutHandler logoutHandler;
     private static final String[] WHITELIST = {
         "/api/v1/auth/**"
     };
@@ -46,6 +49,9 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
+                            .addLogoutHandler(logoutHandler)
+                            .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()))
             .build();
     }
 
@@ -59,6 +65,7 @@ public class SecurityConfig {
 
         corsConfiguration.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
 
+        // provide front-end derver link here
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
 
         corsConfiguration.setAllowCredentials(true);
